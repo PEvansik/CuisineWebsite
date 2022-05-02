2301e42bb268392665d8c9fc4d719d3155d0a834
IKHARO Paul <paulevansikharo@gmail.com>
2022-05-01 23:18:12 -0700

    Pseudo code tweaks

--- JS/main.js
+++ JS/main.js
@@ -4,7 +4,11 @@
 //the element on display will the value on the active DOM it could also be the first in the stack
 //
 //when you click previous,
-    //check the index of the present item on the active DOM  (for mobile)
+    //check the index of the present item on the active DOM  (for mobile) to ensure it is not the first element
     //check the index of the element in the first and last container of the stack (desktop and tablet)
     //decrement the tracker such that the item in the active stack is 
-    //if the current element is 
+    //if the current element is the first element, return
+
+//when you click next,
+    //check the index of the present item on the active DOM (for mobile) to ensure it is not the first element
+    //
