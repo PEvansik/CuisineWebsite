f7a7cda2069ca9a28576cec7e9d161941e400d90
IKHARO Paul <paulevansikharo@gmail.com>
2022-05-01 23:10:46 -0700

    JS pseudo code

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+//get elements in the same category e.g breakfast
+
+//create a varibale to track count (a countup and a countdown to track the element on display)
+//the element on display will the value on the active DOM it could also be the first in the stack
+//
+//when you click previous,
+    //check the index of the present item on the active DOM  (for mobile)
+    //check the index of the element in the first and last container of the stack (desktop and tablet)
+    //decrement the tracker such that the item in the active stack is 
+    //if the current element is 
